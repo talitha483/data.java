aa384141470e3b710865f6a3627eadbb1e139a40
542241732-33smktelmlg <talitha_aurely_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:42:18 +0700

    Add files via upload

---
+++ data.java
@@ -0,0 +1,14 @@
+public class data {
+    public static void main(String[] args) {
+        //tipe_data nama_var = value/isi var;
+        String nama = "talitha";
+        int usia = 15;
+        String alamat = "jl. sunan kudus ponorogo";
+        double kelas = 10.7;
+
+        System.out.println("nama saya : " + nama);
+        System.out.println("usia : " + usia);
+        System.out.println("alamat : " + alamat);
+        System.out.println("kelas : " + kelas);
+    }
+}
