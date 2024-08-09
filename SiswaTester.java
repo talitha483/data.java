5afbeac960963435993b0228be0545c06714c890
542241732-33smktelmlg <talitha_aurely_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 14:21:37 +0700

    Add files via upload

---
+++ SiswaTester.java
@@ -0,0 +1,151 @@
+//Driver Class
+public class SiswaTester {
+    public static void main(String[] args) {
+        //object
+        //Class object = new Constructor
+        siswa desti = new siswa();
+        siswa zea = new siswa();
+        siswa nimas = new siswa();
+        siswa lila = new siswa();
+        siswa sasa = new siswa();
+
+        desti.nama = "Destina";
+        desti.ipk = 91.0;
+        desti.id = 7;
+
+        zea.nama = "Azzahra";
+        zea.ipk = 95.0;
+        zea.id = 6;
+
+        nimas.nama = "Nimasputu";
+        nimas.ipk = 99.0;
+        nimas.id = 30;
+
+        lila.nama = "Kalila";
+        lila.ipk = 94.0;
+        lila.id = 12;
+
+        sasa.nama = "Latisa";
+        sasa.ipk = 90.0;
+        sasa.id = 17;
+
+
+        System.out.println("nama : "+ desti.nama);
+        System.out.println("absen : "+ desti.id);
+        System.out.println("nilai : "+ desti.ipk);
+
+        System.out.println("nama : "+ zea.nama );
+        System.out.println("absen : "+ zea.id);
+        System.out.println("nilai : "+ zea.ipk);
+
+        System.out.println("nama : "+ nimas.nama);
+        System.out.println("absen : "+ nimas.id);
+        System.out.println("nilai : "+ nimas.ipk);
+
+        System.out.println("nama : "+ lila.nama);
+        System.out.println("absen : "+ lila.id);
+        System.out.println("nilai : "+ lila.ipk);
+
+        System.out.println("nama : "+ sasa.nama);
+        System.out.println("absen : "+ sasa.id);
+        System.out.println("nilai : "+ sasa.ipk);
+
+
+
+        
+    }
+}   
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+
+
+
+
+
+         
+
+    
+    
+
