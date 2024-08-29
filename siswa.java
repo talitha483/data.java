9788b71c9a622eab55d2a808adbc5e61b92dd879
542241732-33smktelmlg <talitha_aurely_33rpl@student.smktelkom-mlg.sch.id>
2024-08-29 14:50:30 +0700

    Add files via upload

--- siswa.java
+++ siswa.java
@@ -10,7 +10,24 @@ public class siswa {
         id = 0;
         nama = "kosong";
         ipk = 0.0;
-    }    
-}
+    }  
+    
+    //constructor parameter
+    public siswa(int id, String nama, double
+    ipk) {
+        //variable = parameter
+        this.id = id;
+        this.nama = nama;
+        this.ipk = ipk;
+    }
+
+    //method print
+    public void print() {
+        System.out.println("id : " + id);
+        System.out.println("nama : " + nama);
+        System.out.println("ipk : " + ipk);
+    }
+        
+    }
 
     
